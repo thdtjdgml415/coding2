85d4c3ae535052207f743813bd83b327ae1070a3
thdtjdgml415 <76896490+thdtjdgml415@users.noreply.github.com>
2022-09-12 22:34:45 +0900

    2022-09-12

---
+++ site/site1/assets/js/active.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".side__btn");
+const sideMenu = document.querySelector(".side__menu")
+
+
+btn.addEventListener("click", (a) => {
+    a.preventDefault();
+    sideMenu.classList.toggle("hide");
+    
+
+});
