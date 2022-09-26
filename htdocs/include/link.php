831f415221eadeae18244edd650e9c8a9c1f5aa1
thdtjdgml415 <thdtjdgml415@naver.com>
2022-09-26 09:28:01 +0900

    2022-0926
    다시 가져옴

---
+++ htdocs/include/link.php
@@ -0,0 +1,16 @@
+<!-- css -->
+<link rel="stylesheet" href="../assets/css/style.min.css">
+
+<!-- seo -->
+<meta name="author" content="song">
+<meta name="description" content="눈물의 사이트 만들기 튜토리얼입니다.">
+<meta name="keyword" content="눈물, 사이트, 만들기, 튜토리얼, 기초">
+<meta name="robot" content="all">
+
+<!-- 아이콘 -->
+<link rel="icon" href="../assets/img/icon_256.png" />
+<link rel="shortcut icon" href="../assets/img/icon_256.png" />
+<link rel="icon" type="image/png" sizes="256x256" href="../assets/img/icon_256.png" />
+<link rel="icon" type="image/png" sizes="192x192" href="../assets/img/icon_192.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="../assets/img/icon_32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="../assets/img/icon_16.png" />
