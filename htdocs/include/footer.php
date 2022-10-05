595aff80dad0c1751c00e03505438455ca4a614d
thdtjdgml415 <thdtjdgml415@naver.com>
2022-10-05 20:14:41 +0900

    2232

--- htdocs/include/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="footer">
-        <h2>푸터 영역입니다.</h2>
-        <div class="footer__inner container">
-            <address>Copyright @2022 SONG</address>
-            <div>gogogogogogoo</div>
-        </div>
-    </footer>
-    <!-- footer -->
